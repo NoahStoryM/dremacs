144dcffa9e06e9e74efaa3019c294f9cb4281bef
NoahStoryM <noahstorym@gmail.com>
2025-12-13 16:03:08 +0800

    Make sure `DrEmacs` works with Emacs 29 or newer

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;;; init.el --- System Initialization File -*- lexical-binding: t -*-
 
+(when (< emacs-major-version 29)
+  (error "DrEmacs only works with Emacs 29 and newer"))
+
 (let* ((scope-path (expand-file-name "pkgs" user-emacs-directory))
        (meta-path (expand-file-name "meta" scope-path))
        (meta-main-path (expand-file-name "main" meta-path)))
